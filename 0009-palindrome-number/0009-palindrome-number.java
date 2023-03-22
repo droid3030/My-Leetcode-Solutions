f31ba3673b347ef514ed6c1dcffc47fcb7a4f668
droid3030 <100879754+droid3030@users.noreply.github.com>
2023-03-22 17:55:11 -0400

    Time: 9 ms (97.83%), Space: 42.3 MB (27.00%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -5,9 +5,11 @@ class Solution {
         int reversed = 0;
         int remainder = 0;
         int original = x;
+        
         while(x != 0) {
-            remainder = x % 10;
-            reversed = reversed * 10 + remainder;
+            remainder = x % 10; // reversed integer is stored in variable
+            reversed = reversed * 10 + remainder; //multiply reversed by 10 then add the remainder so it gets stored at next decimal place.
+            
             x /= 10;
             
             
